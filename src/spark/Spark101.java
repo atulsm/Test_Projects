32683e6658035f31e9688deee067310a44486ff5
Atul Soman <atul.soman@microfocus.com>
2015-10-29 13:06:44 +0530

    minor changes

--- src/spark/Spark101.java
+++ src/spark/Spark101.java
@@ -1,14 +1,38 @@
 package spark;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
-public class SimpleRDD {
+/**
+ * First standalone spark application
+ * @author satul
+ *
+ */
+public class Spark101 {
 
+	private static List<String> data = Arrays.asList(new String[] 
+			{
+					"my name is atul soman",
+					"my name is alen atul",
+					"evan atul",
+					"anjana paulose"
+			}); 
+	
 	public static void main(String[] args) {
 		
 		SparkConf sparkConf = new SparkConf();
+		sparkConf.setMaster("local");
+		sparkConf.setAppName("TestSpark");
 		
+		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+		JavaRDD<String> input = sc.parallelize(data);
 
+		String out =  input.first();
+		System.out.println(out);
 	}
 
 }
