71206146333be1f93072a05390646e14be596cbf
Atul Soman <atul.soman@microfocus.com>
2016-02-16 20:10:58 +0530

    Enhancement to generate random ip and veriying EPS

--- src/Client.java
+++ src/Client.java
@@ -20,8 +20,11 @@ import javax.net.ssl.X509TrustManager;
 
 public class Client {	
 	private static final Random rnd = new Random();
-	private static int EPS =1;
-	private static final int NUM_THREADS = 5; 
+	private static int EPS =5;
+	private static double EPS_VARIENCE =0.5;
+	private static int EPS_STICKY_TIME =5;	
+	
+	private static final int NUM_THREADS = 3; 
 	private static final int NUM_ES_PER_COLLECTOR = 1;
 
 	//private static final String SERVER = "192.168.56.101";
@@ -29,8 +32,8 @@ public class Client {
 	private static final int PORT = 1468;
 	private static final boolean ssl = false;
 	
-	private static boolean sendOldData = true;
-	private static final int sendFromDate = 60; //Send events dating from this day before today.
+	private static boolean sendOldData = false;
+	private static final int sendFromDate = 30; //Send events dating from this day before today.
 	private static final int sendToDate = 0; //Send events dating from this day before today.
 
 	
@@ -43,18 +46,33 @@ public class Client {
 	
 	
 	private static final String[] msg = {	
-			"snort[4323]: [1:472:5] ICMP redirect host [Classification: Potentially Bad Traffic] [Priority: 2]: {ICMP} 41.66.0.0 -> 1.6.0.0 ", //Snort
+			
+			"snort[4323]: [1:472:5] ICMP redirect host [Classification: Potentially Bad Traffic] [Priority: 2]: {ICMP} #IP1 -> #IP2 ", //Snort
+			
+			"sshd[28817]: error: PAM: Authentication failure for user_tena1 from #IP1",		
 
-			"sshd[28817]: error: PAM: Authentication failure for user_tena1 from 23.16.0.1",		
+			//"sshd[28817]: error: PAM: Authentication failure for user_tena1 from 23.16.0.1",
+			
+			"MSWinEventLog\t1\tSecurity\t78\tMon Aug 11 21:01:44 2014\t4624\tMicrosoft-Windows-Security-Auditing\tATUL-SAM\\Administrator\tN/A\tSuccess Audit\tATUL-SAM\tLogon\t\tAn account was successfully logged on.    Subject:   Security ID:  S-1-5-18   Account Name:  ATUL-SAM$   Account Domain:  WORKGROUP   Logon ID:  0x3e7    Logon Type:   10    New Logon:   Security ID:  S-1-5-21-976411532-455443073-982928273-500   Account Name:  Administrator   Account Domain:  ATUL-SAM   Logon ID:  0xb34c7   Logon GUID:  {00000000-0000-0000-0000-000000000000}    Process Information:   Process ID:  0x101c   Process Name:  C:\\Windows\\System32\\winlogon.exe    Network Information:   Workstation Name: ATUL-SAM   Source Network Address: 164.99.202.15   Source Port:  2506    Detailed Authentication Information:   Logon Process:  User32    Authentication Package: Negotiate   Transited Services: -   Package Name (NTLM only): -   Key Length:  0    This event is generated when a logon session is created. It is generated on the computer that was accessed.    The subject fields indicate the account on the local system which requested the logon. This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.    The logon type field indicates the kind of logon that occurred. The most common types are 2 (interactive) and 3 (network).    The New Logon fields indicate the account for whom the new logon was created, i.e. the account that was logged on.    The network fields indicate where a remote logon request originated. Workstation name is not always available and may be left blank in some cases.    The authentication information fields provide detailed information about this specific logon request.   - Logon GUID is a unique identifier that can be used to correlate this event with a KDC event.   - Transited services indicate which intermediate services have participated in this logon request.   - Package name indicates which sub-protocol was used among the NTLM protocols.   - Key length indicates the length of the generated session key. This will be 0 if no session key was requested.  This event is generated when a logon session is created. It is generated on the computer that was accessed.    The subject fields indicate the account on the local system which requested the logon. This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.    The logon type field indicates the kind of logon that occurred. The most common types are 2 (interactive) and 3 (network).    The New Logon fields indicate the account for whom the new logon was created, i.e. the account that was logged on.    The network fields indicate where a remote logon request originated. Workstation name is not always available and may be left blank in some cases.    The authentication information fields provide detailed information about this specific logon request.   - Logon GUID is a unique identifier that can be used to correlate this event with a KDC event.   - Transited services indicate which intermediate services have participated in this logon request.   - Package name indicates which sub-protocol was used among the NTLM protocols.   - Key length indicates the length of the generated session key. This will be 0 if no session key was requested. This event is generated when a logon session is created. It is generated on the computer that was accessed.    The subject fields indicate the account on the local system which requested the logon. This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.    The logon type field indicates the kind of logon that occurred. The most common types are 2 (interactive) and 3 (network).    The New Logon fields indicate the account for whom the new logon was created, i.e. the account that was logged on.    The network fields indicate where a remote logon request originated. \t57", //Windows collector 4000 chars
+			"usermod[32277]: delete `Testuser2' from group `newgroup1'", //RedHat Linux
+			"%ASA-6-302014: Teardown TCP connection 7177 for management:164.99.17.109/42257 to NP Identity Ifc:164.99.17.6/443 duration 0:00:00 bytes 2698 TCP FINs", //CISCO
+
+			
+			//"snort[4323]: [1:472:5] ICMP redirect host [Classification: Potentially Bad Traffic] [Priority: 2]: {ICMP} 172.16.3.10 -> 172.16.1.177 ", //Snort
+
+
+			//"snort[4323]: [1:472:5] ICMP redirect host [Classification: Potentially Bad Traffic] [Priority: 2]: {ICMP} 41.66.0.0 -> 1.6.0.0 ", //Snort
+			
+
+			//"sshd[28817]: error: PAM: Authentication failure for user_tena1 from 23.16.0.1",		
 
 
 		"httpd: GET /tomcat.css HTTP/1.1 200 5926",
 		"kernel: SUSE Linux Enterprise Server 10 (x86_64)", //SUSE Collector	
-		"sshd[28817]: error: PAM: Authentication failure for satul from 1.6.0.0", //Generic INDIA  			
-		"snort[4323]: [1:472:5] ICMP redirect host [Classification: Potentially Bad Traffic] [Priority: 2]: {ICMP} 172.16.3.10 -> 172.16.1.177 ", //Snort
+		//"sshd[28817]: error: PAM: Authentication failure for satul from 1.6.0.0", //Generic INDIA  			
+		//"snort[4323]: [1:472:5] ICMP redirect host [Classification: Potentially Bad Traffic] [Priority: 2]: {ICMP} 172.16.3.10 -> 172.16.1.177 ", //Snort
 		
-		"sshd[28817]: error: PAM: Authentication failure for user_tena1 from 23.16.0.1",		
-		"MSWinEventLog\t1\tSecurity\t78\tMon Aug 11 21:01:44 2014\t4624\tMicrosoft-Windows-Security-Auditing\tATUL-SAM\\Administrator\tN/A\tSuccess Audit\tATUL-SAM\tLogon\t\tAn account was successfully logged on.    Subject:   Security ID:  S-1-5-18   Account Name:  ATUL-SAM$   Account Domain:  WORKGROUP   Logon ID:  0x3e7    Logon Type:   10    New Logon:   Security ID:  S-1-5-21-976411532-455443073-982928273-500   Account Name:  Administrator   Account Domain:  ATUL-SAM   Logon ID:  0xb34c7   Logon GUID:  {00000000-0000-0000-0000-000000000000}    Process Information:   Process ID:  0x101c   Process Name:  C:\\Windows\\System32\\winlogon.exe    Network Information:   Workstation Name: ATUL-SAM   Source Network Address: 164.99.202.15   Source Port:  2506    Detailed Authentication Information:   Logon Process:  User32    Authentication Package: Negotiate   Transited Services: -   Package Name (NTLM only): -   Key Length:  0    This event is generated when a logon session is created. It is generated on the computer that was accessed.    The subject fields indicate the account on the local system which requested the logon. This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.    The logon type field indicates the kind of logon that occurred. The most common types are 2 (interactive) and 3 (network).    The New Logon fields indicate the account for whom the new logon was created, i.e. the account that was logged on.    The network fields indicate where a remote logon request originated. Workstation name is not always available and may be left blank in some cases.    The authentication information fields provide detailed information about this specific logon request.   - Logon GUID is a unique identifier that can be used to correlate this event with a KDC event.   - Transited services indicate which intermediate services have participated in this logon request.   - Package name indicates which sub-protocol was used among the NTLM protocols.   - Key length indicates the length of the generated session key. This will be 0 if no session key was requested.  This event is generated when a logon session is created. It is generated on the computer that was accessed.    The subject fields indicate the account on the local system which requested the logon. This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.    The logon type field indicates the kind of logon that occurred. The most common types are 2 (interactive) and 3 (network).    The New Logon fields indicate the account for whom the new logon was created, i.e. the account that was logged on.    The network fields indicate where a remote logon request originated. Workstation name is not always available and may be left blank in some cases.    The authentication information fields provide detailed information about this specific logon request.   - Logon GUID is a unique identifier that can be used to correlate this event with a KDC event.   - Transited services indicate which intermediate services have participated in this logon request.   - Package name indicates which sub-protocol was used among the NTLM protocols.   - Key length indicates the length of the generated session key. This will be 0 if no session key was requested. This event is generated when a logon session is created. It is generated on the computer that was accessed.    The subject fields indicate the account on the local system which requested the logon. This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.    The logon type field indicates the kind of logon that occurred. The most common types are 2 (interactive) and 3 (network).    The New Logon fields indicate the account for whom the new logon was created, i.e. the account that was logged on.    The network fields indicate where a remote logon request originated. \t57", //Windows collector 4000 chars
+		//"sshd[28817]: error: PAM: Authentication failure for user_tena1 from 23.16.0.1",		
 		"%ASA-6-302014: Teardown TCP connection 7177 for management:164.99.17.109/42257 to NP Identity Ifc:164.99.17.6/443 duration 0:00:00 bytes 2698 TCP FINs", //CISCO
 
 
@@ -79,6 +97,9 @@ public class Client {
 	private static int threadNum =0;
 	private static AtomicLong globalCount = new AtomicLong(2000);
 	private static long MAX_EVENTS = Long.MAX_VALUE;
+	
+	private static long lastEPSChange = System.currentTimeMillis();
+	private static long lastEPS = EPS;
 	
 	static{
 		Runtime.getRuntime().addShutdownHook(new Thread(){
@@ -116,12 +137,19 @@ public class Client {
 						int localCount = 0;
 						while(true){
 							for(int j=0;j<NUM_ES_PER_COLLECTOR;j++){	
+								String msg = null;
 								if(!sendOldData){
-									writer.write("MachineTest2"+"."+j+" " +message +  " AtulCount" + globalCount.getAndIncrement() + "\n");
+									//writer.write("MachineTest"+j+" " +message +  " AtulCount" + globalCount.getAndIncrement() + "\n");
+									//writer.write("MachineTest"+globalCount.getAndIncrement()+" " +message +  " AtulCount" + globalCount.getAndIncrement() + "\n");
+									msg = "MachineTest"+j+" " +message +  " AtulHost" + globalCount.getAndIncrement() + "\n";																	
 								}else{
-									writer.write(getDate() + " MachineTest2"+"."+j+" " +  message +  " AtulCount" + globalCount.getAndIncrement() + "\n");
+									msg = getDate() + " MachineTest2"+"."+j+" " +  message +  " AtulCount" + globalCount.getAndIncrement() + "\n";
 								}
 								
+								msg = msg.replace("#IP1", getRandomIP());
+								msg = msg.replace("#IP2", getRandomIP());
+								
+								writer.write(msg);
 								writer.flush();
 								
 								if(globalCount.get() == MAX_EVENTS){
@@ -140,7 +168,7 @@ public class Client {
 							if((time1-time) >= 1000){
 								time = System.currentTimeMillis();
 								localCount=0;
-							}else if(localCount > EPS){
+							}else if(localCount > getEPS()){
 								sleep(1100 - (time1 - time));
 								localCount = 0;
 							}
@@ -152,6 +180,28 @@ public class Client {
 				}
 			}.start();			
 		}		
+	}
+	
+	private static long getEPS(){
+		if(EPS_VARIENCE == 1.0){		
+			return EPS;
+		}else{
+			if( (System.currentTimeMillis() - lastEPSChange) > EPS_STICKY_TIME*1000){
+				if(getRandomDay(0, 2) == 1){
+					lastEPS = Math.round(lastEPS + (lastEPS*(1-EPS_VARIENCE)));
+				}else{
+					lastEPS = Math.round(lastEPS - (lastEPS*(1-EPS_VARIENCE)));
+				}
+				
+				if(lastEPS < EPS/10 || lastEPS > EPS*10 || EPS == 0){
+					lastEPS=EPS;
+				}
+				System.out.println("EPS changed to " + lastEPS);
+				lastEPSChange = System.currentTimeMillis();
+			}			
+			return lastEPS;
+			
+		}
 	}
 	
 	private static Socket getSSLSocket(){
@@ -255,5 +305,10 @@ public class Client {
 	private static  int getRandomDay(int start, int end){
 		int R = r.nextInt(end-start) + start;
 		return R;
+	}
+	
+	private static String getRandomIP(){
+		Random r = new Random();
+		return r.nextInt(256) + "." + r.nextInt(256) + "." + r.nextInt(256) + "." + r.nextInt(256);
 	}
 }
