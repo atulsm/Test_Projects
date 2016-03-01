87ea6cb138107a8599540f63a2e6a579f5b405b4
Atul Soman <atul.soman@microfocus.com>
2016-03-01 23:23:14 +0530

    Add test for xee

--- src/TestDate.java
+++ src/TestDate.java
@@ -14,7 +14,8 @@ public class TestDate {
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		
-		
+		System.out.println(TimeZone.getDefault().getRawOffset());
+		System.out.println(330*60*1000);
 		
 		cal.setTimeInMillis(System.currentTimeMillis() + 5*60*60*1000);
 		
@@ -27,6 +28,14 @@ public class TestDate {
 		System.out.println(new Date((1381883504425581000L)/1000000));
 				
 		System.out.println(agentDateFormat.format(cal.getTime()));
+		
+		TimeZone defaultTZ = TimeZone.getDefault();
+		
+		TimeZone utc = TimeZone.getTimeZone("UTC");
+		Calendar calUtc = Calendar.getInstance(utc);
+		long diff = calUtc.getTime().getTime() - new Date().getTime();
+
+
 	}
 
 }
