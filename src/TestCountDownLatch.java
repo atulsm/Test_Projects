abca40a21b84c1a7d96d6714d1c499a605040e5e
Atul Soman <atul.soman@microfocus.com>
2019-03-19 10:50:03 +0530

    Fixing countdownlatch implementation

--- src/TestCountDownLatch.java
+++ src/TestCountDownLatch.java
@@ -3,19 +3,38 @@ import java.util.concurrent.CountDownLatch;
 public class TestCountDownLatch {
 	
 	public static void main(String[] args) {
-		final CountDownLatch startLatch = new CountDownLatch(1);
+		final CountDownLatch startLatch = new CountDownLatch(4);
+		
+		new Thread() {
+			public void run() {
+				System.out.println("Awaiting all latches to finish counting down");
+				try {
+					startLatch.await();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Flood gate is open");
+			};
+		}.start();
+		
 		for (int threadNo = 0; threadNo < 4; threadNo++) {
 		  new Thread(){
 			  public void run() {
 				  try {
-					startLatch.await();
-					System.out.println(System.nanoTime());
-					
+					startLatch.countDown();
+					System.out.println("Waiting for countDown " + System.nanoTime());
 				} catch (Exception e) {}
 			  };			  
 		  }.start();
+		  
+		  try {
+			Thread.sleep(2000);
+		  } catch (InterruptedException e) {
+			e.printStackTrace();
+		  }
 		}
-		startLatch.countDown();
+		
+		
 	}
 
 }
